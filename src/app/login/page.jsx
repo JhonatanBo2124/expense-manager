91d60d8d1624fc573aecb8e44c8a2f9b65d850eb
Jhonatan Bolivar <jhonatanbolivar14@gmail.com>
2024-01-03 23:40:43 -0500

    login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,18 +1,37 @@
+'use client'
+
+import { IconEye, IconEyeOff } from '@tabler/icons-react'
+import { useState } from 'react'
 
 export default function login () {
+
+    const [ showPass, setShowPass ] = useState(false)
+
     return(
         <>
-        <div className="w-screen h-screen flex flex-col items-center justify-center gap-6">
-            <h1 className="text-2xl">Expense <span className="font-black">Manager</span></h1>
-            <div className="p-10 flex flex-col items-center border-8 rounded-xl">
-                <p className="pb-20">Sign in</p>
-                <form action="" className="flex flex-col items-center gap-3">
-                    <input className="p-2 border-2" type="text" placeholder="Username" />
-                    <input className="p-2 border-2" type="password" placeholder="Password" />
-                    <button>Login</button>
-                    <a className="p-2 flex gap-5 border-2 rounded-md hover:bg-slate-100" href=""><img src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="" />Login with Google</a>
-                    <a href="">Sign up</a>
-                </form>
+        <div className="font-['Oswald'] h-screen w-screen flex items-center justify-center">
+            <div className="w-3/5 rounded-xl flex bg-slate-100">
+                <img className="w-3/5 rounded-l-xl" src="https://images.unsplash.com/photo-1553729459-efe14ef6055d?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="backgroundLogin" />
+                <div className="flex flex-col items-center justify-center p-8">
+                    <h1 className="text-4xl font-bold">Login</h1>
+                    <div className="w-full p-10 flex flex-col items-center justify-center">
+                        <form action="" className="w-56 flex flex-col justify-center gap-3">
+                            <label htmlFor="">Email</label>
+                            <input className="p-2 border-2 bg-transparent" type="email" placeholder="Username" />
+                            <label htmlFor="Password">Password</label>
+                            <div className='flex justify-center items-center border-2 border-gray-200'>
+                                <input className="w-full p-2 border-none bg-transparent" type={showPass ? "text" : "password"} placeholder="Password" />
+                                <div onClick={ () => setShowPass(!showPass)} className='px-2'>
+                                    {showPass ? <IconEye/> : <IconEyeOff/>}
+                                </div>
+                            </div>
+                            <button type='submit' className="bg-blue-600 text-white p-2 rounded-full hover:bg-blue-800">Login</button>
+                            <p className="w-full text-center">or</p>
+                            <a className="p-2 flex gap-5 border-2 rounded-md hover:bg-slate-200 hover:text-black" href=""><img src="https://freesvg.org/img/1534129544.png" alt="" className="w-6" />Sign in with Google</a>
+                            <a className="text-blue-500 decoration-blue-600" href="/signup">Sign up</a>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="h-screen w-screen flex min-h-screen flex-col items-center justify-center gap-20 p-24">
       <h1>
-        <span className="text-white font-['Oswald'] font-black text-9xl">Expense</span>
-        <span className="text-white font-['Oswald'] font-black text-9xl hover:leading-loose flex flex-col gap-0 p-0 hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-sky-800 transition-all">Manager</span>
+        <span className="font-['Oswald'] font-black text-9xl">Expense</span>
+        <span className="font-['Oswald'] font-black text-9xl hover:leading-loose flex flex-col gap-0 p-0 hover:text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-sky-800 transition-all">Manager</span>
       </h1>
       <div className='flex gap-10'>
-        <a href="/login" className='text-white border-2 rounded-md px-6 py-3'>Login</a>
+        <a href="/login" className='border-2 border-slate-800 rounded-md px-6 py-3'>Login</a>
         <a href="/signup" className='text-white rounded-md px-6 py-3 bg-cyan-600 hover:bg-cyan-900 transition-all'>Sign up</a>
       </div>
     </main>
